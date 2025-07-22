e73a9867e2038f6c99002c7e86a263b1b85be313
KhangTruong1006 <156401203+KhangTruong1006@users.noreply.github.com>
2025-07-22 13:23:48 +0700

    create component for viewing with other component

---
+++ front-end/src/components/WordsComponent.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+        <h1>Words</h1>
+        <table id="id" class="ui celled compact table">
+            <thead>
+                <tr>
+                    <th>  
+                        <i class="united kingdom flag"></i> English
+                    </th>
+                    <th>  
+                        <i class="germany flag"></i> German
+                    </th>
+                    <th>  
+                        <i class="vietnam flag"></i> Vietnamese
+                    </th>
+                    <th colspan="3"></th>
+                </tr>
+            </thead>
+            <tr v-for="(word,i) in words" :key ="i">
+                <td>{{ word.english }}</td>
+                <td>{{ word.german }}</td>
+                <td>{{ word.vietnamese }}</td>
+                <td width="75" class="center aligned">
+                    <router-link :to="{name: 'edit',params: {id: word._id}}">Edit</router-link>
+                </td>
+                <td width="75" class="center aligned ">
+                    <button type="button" class="btn btn-danger"  @click.prevent="onDestroy(word._id)" href="`/words/${word._id}">Delete</button>
+                </td>
+
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+    import {api} from '../helpers/helpers.js'
+
+    export default {
+        name: 'words',
+        data() {
+            return {
+                words: []
+            };
+        },
+
+        methods: {
+            async onDestroy(id) {
+                const sure = window.confirm('Are you sure?');
+                if(!sure) return;
+                await api.deleteWord(id);
+                this.flash('Word deleted successfully','success');
+                const newWords = this.words.filter(word => word._id !== id);
+                this.words = newWords;
+            }
+        },
+
+        async mounted() {
+            this.words = await api.getWords();
+        }
+    }
+</script>
